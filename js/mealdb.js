80013394326b058fa54507a7810d8ddaccde1e59
nurulamin092 <mnurulamin092@gmial.com>
2022-02-24 12:35:26 +0600

    search result ok

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,8 +1,6 @@
 const searchFood = () => {
-
     const searchField = document.getElementById('search-input');
     const searchText = searchField.value;
-
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
         .then(res => res.json())
@@ -11,5 +9,19 @@ const searchFood = () => {
 }
 
 const displaySearchResult = meals => {
-    console.log(meals);
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = ` <div class="card h-100">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                additional content.</p>
+        </div>
+    </div>`;
+        searchResult.appendChild(div)
+        console.log(meal);
+    });
 }
