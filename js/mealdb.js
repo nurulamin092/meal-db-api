bf04b19e629f65b3b8bb9b3422319f506f7c776f
nurulamin092 <mnurulamin092@gmial.com>
2022-02-24 16:28:22 +0600

    show meal id details

--- js/mealdb.js
+++ js/mealdb.js
@@ -17,8 +17,7 @@ const displaySearchResult = meals => {
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                additional content.</p>
+            <p class="card-text">${meal.strInstructions.slice(0, 250)}</p>
         </div>
     </div>`;
         searchResult.appendChild(div)
@@ -28,8 +27,23 @@ const displaySearchResult = meals => {
 
 const loadMealDetails = mealId => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals))
+        .then(data => displayMealDetails(data.meals[0]))
+}
+
+const displayMealDetails = meal => {
+    const mealDetails = document.getElementById('meal-details');
+
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `  <div class="card">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions}</p>
+        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>`
+    mealDetails.appendChild(div);
 }
