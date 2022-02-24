9eb40e4dd43de726f7339de916a67df32b3fd38e
nurulamin092 <mnurulamin092@gmial.com>
2022-02-24 19:33:08 +0600

    add asynconus function

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,10 +1,15 @@
-const searchFood = () => {
+const searchFood = async () => {
     const searchField = document.getElementById('search-input');
     const searchText = searchField.value;
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    fetch(url)
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displaySearchResult(data.meals)
+    /*  fetch(url)
         .then(res => res.json())
-        .then(data => displaySearchResult(data.meals))
+        .then(data => displaySearchResult(data.meals)) 
+ */
     searchField.value = '';
 }
 
@@ -26,11 +31,15 @@ const displaySearchResult = meals => {
     });
 }
 
-const loadMealDetails = mealId => {
+const loadMealDetails = async mealId => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-    fetch(url)
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMealDetails(data.meals[0])
+    /* fetch(url)
         .then(res => res.json())
-        .then(data => displayMealDetails(data.meals[0]))
+        .then(data => displayMealDetails(data.meals[0])); */
 }
 
 const displayMealDetails = meal => {
@@ -42,7 +51,7 @@ const displayMealDetails = meal => {
     <img src="${meal.strMealThumb}" class="card-img-top" alt="">
     <div class="card-body">
         <h5 class="card-title">${meal.strMeal}</h5>
-        <p class="card-text">${meal.strInstructions}</p>
+        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
         <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
     </div>
 </div>`
