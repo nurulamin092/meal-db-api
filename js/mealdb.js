718f4e8cf11989c642b3419f9e6c04fb2d74a726
nurulamin092 <mnurulamin092@gmial.com>
2022-02-24 12:22:58 +0600

    daynamic url api add

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,3 +1,15 @@
 const searchFood = () => {
 
+    const searchField = document.getElementById('search-input');
+    const searchText = searchField.value;
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.meals))
+    searchField.value = '';
+}
+
+const displaySearchResult = meals => {
+    console.log(meals);
 }
